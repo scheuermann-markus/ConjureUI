5c5d883bc2ebe1fe872089c9f8b3379af8c0e2d0
Markus <scheuermann-markus@gmx.de>
2025-02-01 16:04:41 +0100

    [main] Implemented YesNoCancel Modal

--- src/app/ConfirmModal/page.tsx
+++ src/app/ConfirmModal/page.tsx
@@ -18,17 +18,43 @@ export default function Page()
         }
     }
 
+    const openYesNoCancelModal = async () =>
+    {
+        const userResponse = await _useConfirmModal.openYesNoCancelModal(`Any text you want...`);
+
+        switch (userResponse.action) {
+            case ModalAction.YES:
+                // Add logic when yes
+                break;
+            case ModalAction.NO:
+                // Add logic when no
+                break;
+            case ModalAction.CANCEL:
+                // Add logic when cancel
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <section className="mx-32 mt-10 text-whitesmoke">
             <h1 className="text-3xl">Confirm Modal</h1>
             <hr className="border-0 border-t-2 mt-2 mb-4"/>
 
-            <div>
+            <div className="flex flex-col gap-4 w-fit">
                 <button
                     onClick={openDeleteConfirm}
                     className="cursor-pointer border border-white rounded-[6px] bg-transparent hover:bg-white hover:text-black transition-colors duration-200 px-4 py-2 select-none"
                 >
                     Show Delete Confirm
+                </button>
+
+                <button
+                    onClick={openYesNoCancelModal}
+                    className="cursor-pointer border border-white rounded-[6px] bg-transparent hover:bg-white hover:text-black transition-colors duration-200 px-4 py-2 select-none"
+                >
+                    Show Yes-No-Cancel Modal
                 </button>
             </div>
 
