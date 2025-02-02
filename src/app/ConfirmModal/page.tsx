a06c2b889f4c9eb8c4d1e47df15d286d303c5269
Markus <scheuermann-markus@gmx.de>
2025-02-02 14:49:32 +0100

    [main] Added DeleteConfirmEditor

--- src/app/ConfirmModal/page.tsx
+++ src/app/ConfirmModal/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useConfirmModal, ModalAction } from "@/components/ConfirmModal";
+import DeleteConfirmEditor from "@/app/ConfirmModal/components/DeleteConfirmEditor";
 
 export default function Page()
 {
@@ -22,7 +23,8 @@ export default function Page()
     {
         const userResponse = await _useConfirmModal.openYesNoCancelModal(`Any text you want...`);
 
-        switch (userResponse.action) {
+        switch (userResponse.action)
+        {
             case ModalAction.YES:
                 // Add logic when yes
                 break;
@@ -40,23 +42,30 @@ export default function Page()
     return (
         <section className="mx-32 mt-10 text-whitesmoke">
             <h1 className="text-3xl">Confirm Modal</h1>
-            <hr className="border-0 border-t-2 mt-2 mb-4"/>
+            <hr className="border-0 border-t-2 mt-2 mb-12"/>
 
-            <div className="flex flex-col gap-4 w-fit">
-                <button
-                    onClick={openDeleteConfirm}
-                    className="cursor-pointer border border-white rounded-[6px] bg-transparent hover:bg-white hover:text-black transition-colors duration-200 px-4 py-2 select-none"
-                >
-                    Show Delete Confirm
-                </button>
+            <div className="flex w-full">
+                <div className="w-1/2 flex items-center justify-center">
+                    <button
+                        onClick={openDeleteConfirm}
+                        className="cursor-pointer border border-white rounded-[6px] bg-transparent hover:bg-white hover:text-black transition-colors duration-200 px-4 py-2 select-none"
+                    >
+                        Show Delete Confirm
+                    </button>
+                </div>
 
-                <button
-                    onClick={openYesNoCancelModal}
-                    className="cursor-pointer border border-white rounded-[6px] bg-transparent hover:bg-white hover:text-black transition-colors duration-200 px-4 py-2 select-none"
-                >
-                    Show Yes-No-Cancel Modal
-                </button>
+                <div className="w-1/2">
+                    <DeleteConfirmEditor/>
+                </div>
             </div>
+
+
+            <button
+                onClick={openYesNoCancelModal}
+                className="cursor-pointer border border-white rounded-[6px] bg-transparent hover:bg-white hover:text-black transition-colors duration-200 px-4 py-2 select-none"
+            >
+                Show Yes-No-Cancel Modal
+            </button>
 
         </section>
     );
